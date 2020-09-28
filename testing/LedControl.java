4e002c0048e45ddda49a779b6f877ea45532db49
BJPrice01 <n3rdtp@gmail.com>
2020-09-28 17:27:19 -0500

    updated LedControl.java

--- testing/LedControl.java
+++ testing/LedControl.java
@@ -3,7 +3,8 @@
 *Created 09/28/2020
 *Used to get the experience of controlling an led outside of the final product
 */
-
+import java.util.*;
+import com.pi4j.io.gpio.*;
 public class LedControl {
 //Functions
 
@@ -11,5 +12,16 @@ public class LedControl {
 
 //////MAIN//////
   public static void main(String[] args){
+    final GpioController gpio = GpioFactory.getInstance();
+    final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_22);
+    pin.high();
+    System.out.println("PinHigh");
+    Thread.sleep(2000);
+    pin.low();
+    System.out.println("PinLow");
+    Thread.sleep(2000);
+    System.out.println("PinHigh for 2 seconds");
+    pin.pulse(2000, true);
+    gpio.shutdown();
   }
 }
