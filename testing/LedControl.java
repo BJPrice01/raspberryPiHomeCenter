da7808272cf8dbe37cdb34b477097ff48f977557
BJPrice01 <n3rdtp@gmail.com>
2020-09-28 20:07:52 -0500

    changed method to control gpio

--- testing/LedControl.java
+++ testing/LedControl.java
@@ -3,24 +3,16 @@
 *Created 09/28/2020
 *Used to get the experience of controlling an led outside of the final product
 */
-import com.pi4j.io.gpio.*;
+import java.lang.Runtime;
 public class LedControl {
 //Functions
-
-//Classes
+public static void ledSetup(){
+  
+}
 
 //////MAIN//////
   public static void main(String[] args) throws InterruptedException{
-    final GpioController gpio = GpioFactory.getInstance();
-    final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_22);
-    pin.high();
-    System.out.println("PinHigh");
-    Thread.sleep(2000);
-    pin.low();
-    System.out.println("PinLow");
-    Thread.sleep(2000);
-    System.out.println("PinHigh for 2 seconds");
-    pin.pulse(2000, true);
-    gpio.shutdown();
+    Runtime run = new Runtime();
+    run.exec("python lightOn.py");
   }
 }
