a7033d886eaae9f505738f4fe15e6f5034f7bea6
BJPrice01 <n3rdtp@gmail.com>
2020-09-28 20:13:12 -0500

    fixes

--- testing/LedControl.java
+++ testing/LedControl.java
@@ -3,7 +3,7 @@
 *Created 09/28/2020
 *Used to get the experience of controlling an led outside of the final product
 */
-import java.lang.Runtime;
+import java.io.IOException;
 public class LedControl {
 //Functions
 public static void ledSetup(){
@@ -11,8 +11,10 @@ public static void ledSetup(){
 }
 
 //////MAIN//////
-  public static void main(String[] args) throws InterruptedException{
-    Runtime run = new Runtime();
+  public static void main(String[] args) throws IOException, InterruptedException{
+    Runtime run = Runtime.getRuntime();
     run.exec("python lightOn.py");
+    Thread.sleep(1000);
+    run.exec("python lightOff.py");
   }
 }
